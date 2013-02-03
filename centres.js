9d7b9592da962dbe5333f4cca54cee64e7a78472
Joan Creus <joan.creus.c@gmail.com>
2013-02-03 13:19:10 +0100

    Primer commit...

---
+++ centres.js
@@ -0,0 +1,30 @@
+var centres = [
+	{
+		"name": "Frederic Mistral - Tècnic Eulàlia",
+		"itineraris": [
+			{"name": "Tecnològic 1", "modalitats":
+				["Matemàtiques", "Física", "Química", "Economia de l'empresa", "Tecnologia industrial", "Dibuix tècnic"]},
+			{"name": "Tecnològic 2", "modalitats":
+				["Matemàtiques", "Física", "Dibuix tècnic", "Tecnologia industrial"]},
+			{"name": "Científic", "modalitats":
+				["Matemàtiques", "Química", "Biologia", "Física", "Ciències de la Terra i mediambientals"]},
+			{"name": "Ciències socials 1", "modalitats":
+				["Matemàtiques aplicades a les CC.SS", "Economia de l'empresa", "Geografia", "Llatí", "Cultura audiovisual"]},
+			{"name": "Ciències socials 2", "modalitats":
+				["Matemàtiques aplicades a les CC.SS", "Literatura catalana", "Cultura audiovisual"]},
+			{"name": "Humanitats", "modalitats":
+				["Llatí", "Literatura catalana", "Història de l'art", "Geografia", "Cultura audiovisual"]},
+			{"name": "Arts", "modalitats":
+				["Dibuix artístic", "Disseny", "Història de l'art", "Cultura audiovisual"]}
+		]
+	},
+	{
+		"name": "Institut Martí Franquès",
+		"itineraris": [
+			{"name": "Tecnològic", "modalitats": ["Matemàtiques", "Física", "Química"]},
+			{"name": "Ciències de la Naturalesa i la Salut", "modalitats": ["Biologia", "Química", "Matemàtiques"]},
+			{"name": "Ciències socials", "modalitats": ["Matemàtiques aplicades a les CC.SS", "Literatura catalana", "Literatura castellana", "Georgrafia"]},
+			{"name": "Humanístic", "modalitats": ["Llatí", "Literatura catalana", "Literatura castellana", "Història de l'art"]}
+		]
+	}
+];
